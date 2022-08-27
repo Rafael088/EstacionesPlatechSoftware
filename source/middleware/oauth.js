a3a07f2f8faefb04120f2d11c1003453b3822f96
diego axsel garcia <diego.garciasi@autonoma.edu.co>
2022-08-26 22:14:11 -0500

    fix : oauth middleware

--- source/middleware/oauth.js
+++ source/middleware/oauth.js
@@ -1,23 +1,19 @@
+const jose = require('jose')
+
 const OAuth = (req, res, next) => {
-  
+
     try{
         const token = req.header('Authorization').replace('Bearer ', '');
         const secret = new TextEncoder().encode(
             process.env.JWT_SECRET
         );
-    
-        try {
 
-            jose.jwtVerify(token, secret)
+        jose.jwtVerify(token, secret)
             .then(data => next())
             .catch((error) => res.status(400).send("token invalido"))
 
-            
-          } catch (e) {
-            res.send("Token invalido");
-          }
     }catch(e){
-        res.status(500).send("no se presento token")
+        res.status(500).send("error en la validacion")
     }
 }
 
--- source/utils/autoRouter/autoRouter.js
+++ source/utils/autoRouter/autoRouter.js
@@ -6,10 +6,12 @@ var router = express.Router();
 const autoRouter = (indexer, services, middleware) => {
     indexer.forEach((value, index) => {
 
-        if (Object.keys(value).length == 2){
+        if (Object.keys(value).length == 2 && value.service != "oauth"){
             router?.[value.method]("/" + value.service, OAuth ,(req, res) => services?.[value.service](req,res))
-        }else{
+        }else if(Object.keys(value).length == 3){
             router?.[value.method]("/" + value.service,  [OAuth, middleware?.[value.middleware]] ,(req, res) => services?.[value.service](req,res))
+        }else{
+            router?.[value.method]("/" + value.service ,(req, res) => services?.[value.service](req,res))
         }
     })
 
